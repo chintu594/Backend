7194066e39797b9fdede5d448202e770d50ef7ee
chintu594 <chintumewara816@gmail.com>
2026-03-07 11:42:25 +0530

    fix backend errors

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const Post = require("../models/Post");
+const Post = require("../models/post");
 
 router.get("/", async (req, res) => {
-    try {
-        const posts = await Post.find()/* .sort({ _id: -1 } )*/; // latest first
-        res.json(posts);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 router.post("/", async (req, res) => {
-    try {
-        const saved = await Post.create(req.body);
-        res.status(201).json(saved);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+  try {
+    const saved = await Post.create(req.body);
+    res.status(201).json(saved);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 module.exports = router;
